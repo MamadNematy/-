08e1eb763d8ddbb0b9c1bb40d66826c5fdc811bc
MamadNematy <mj2864250@gmail.com>
2025-05-21 19:43:28 +0330

    fix images folders

--- js/products.js
+++ js/products.js
@@ -3,14 +3,14 @@ const products = [
     id: 1,
     name: "کراس ترینر PulseFitness",
     price: 76_850_000,
-    image: "assets/images/cross-trainer-1.jpg",
+    image: "assets/images/products/cross-trainer-1.jpg",
     description: "دستگاه هوازی روان و کم‌فشار، مناسب چربی‌سوزی بدون آسیب مفصلی",
   },
   {
     id: 2,
     name: "دستگاه اسمیت PulseFitness",
     price: 43_000_000,
-    image: "assets/images/smith-machine-2.jpg",
+    image: "assets/images/products/smith-machine-2.jpg",
     description:
       "دستگاه چندکاره با ریل ثابت، مناسب تمرین پرس و اسکوات ایمن و حرفه‌ای",
   },
@@ -18,7 +18,7 @@ const products = [
     id: 3,
     name: "تردمیل باشگاهی PulseFitness",
     price: 54_000_000,
-    image: "assets/images/Treadmill-3.jpg",
+    image: "assets/images/products/Treadmill-3.jpg",
     description:
       "موتور پرقدرت با شیب برقی، مناسب تمرین‌های سنگین و مداوم در باشگاه",
   },
@@ -26,7 +26,7 @@ const products = [
     id: 4,
     name: "استپ میل PulseFitness",
     price: 178_000_000,
-    image: "assets/images/stepmill-4.jpg",
+    image: "assets/images/products/stepmill-4.jpg",
     description:
       "دستگاه پله‌نوردی با حرکت مداوم پله‌ها، مناسب تمرین قدرتی-هوازی پایین‌تنه",
   },
@@ -34,7 +34,7 @@ const products = [
     id: 5,
     name: "دستگاه لت PulseFitness",
     price: 35_000_000,
-    image: "assets/images/LatPulldown-5.jpg",
+    image: "assets/images/products/LatPulldown-5.jpg",
     description:
       "مناسب تقویت عضلات پشت و زیربغل با حرکت کنترل‌شده و دامنه کامل",
   },
@@ -42,7 +42,7 @@ const products = [
     id: 6,
     name: "دوچرخه ثابت PulseFitness",
     price: 17_400_000,
-    image: "assets/images/upright-cycle-6.jpg",
+    image: "assets/images/products/upright-cycle-6.jpg",
     description:
       "دوچرخه هوازی با عملکرد روان، مناسب تمرین مداوم قلبی و عضلات پا",
   },
@@ -50,7 +50,7 @@ const products = [
     id: 7,
     name: "کلایمبر کوهنوردی PulseFitness",
     price: 34_000_000,
-    image: "assets/images/Vertical-Climber-7.jpg",
+    image: "assets/images/products/Vertical-Climber-7.jpg",
     description:
       "کلایمبر کوهنوردی با طراحی فشرده و حرکت ترکیبی دست و پا، مناسب تمرین پرفشار هوازی و تقویت هم‌زمان عضلات بالا و پایین‌تنه",
   },
@@ -58,7 +58,7 @@ const products = [
     id: 8,
     name: "دستگاه قایقرانی کتلر PulseFitness",
     price: 42_000_000,
-    image: "assets/images/Indoor-Rower-8.jpg",
+    image: "assets/images/products/Indoor-Rower-8.jpg",
     description:
       "دستگاه قایقرانی کتلر با حرکت روان و مکانیزم مقاومتی دقیق، مناسب تقویت عضلات مرکزی، بالا و پایین‌تنه و بهبود استقامت قلبی",
   },
@@ -66,7 +66,7 @@ const products = [
     id: 9,
     name: "دستگاه پرس سینه هالتر المپیکی",
     price: 44_500_000,
-    image: "assets/images/Olympic-Horizontal-Bench-Press-9.jpg",
+    image: "assets/images/products/Olympic-Horizontal-Bench-Press-9.jpg",
     description:
       "دستگاه پرس سینه هالتر المپیکی با ساختار پایدار و طراحی ارگونومیک، مناسب افزایش حجم و قدرت عضلات سینه، سرشانه و پشت بازو",
   },
@@ -74,7 +74,8 @@ const products = [
     id: 10,
     name: "ست دمبل باشگاهی 2.5 تا 25 کیلوگرم به‌همراه میز دوطبقه",
     price: 65_300_000,
-    image: "assets/images/2.5-25kg-Black-Rubber-Covered-Dumbbell-Set-10.jpg",
+    image:
+      "assets/images/products/2.5-25kg-Black-Rubber-Covered-Dumbbell-Set-10.jpg",
     description:
       "شامل ۱۰ جفت دمبل روکش‌دار و میز فلزی مستحکم برای نگهداری منظم در باشگاه",
   },
@@ -82,7 +83,8 @@ const products = [
     id: 11,
     name: "ست دمبل باشگاهی 27.5 تا 50 کیلوگرم با میز فلزی سنگین‌وزن",
     price: 58_000_000,
-    image: "assets/images/27.5-50kg-Black-Rubber-Covered-Dumbbell-Set-11.jpg",
+    image:
+      "assets/images/products/27.5-50kg-Black-Rubber-Covered-Dumbbell-Set-11.jpg",
     description:
       "مناسب تمرینات قدرتی سنگین، شامل 12 جفت دمبل روکش‌دار با میز نگهدارنده صنعتی",
   },
@@ -90,7 +92,8 @@ const products = [
     id: 12,
     name: "ست دمبل باشگاهی 52.5 تا 60 کیلوگرم با میز نگهدارنده صنعتی",
     price: 53_000_000,
-    image: "assets/images/52.5-60kg-Black-Rubber-Covered-Dumbbell-Set-12.jpg",
+    image:
+      "assets/images/products/52.5-60kg-Black-Rubber-Covered-Dumbbell-Set-12.jpg",
     description:
       "ست سنگین بدنسازی با روکش لاستیکی، مناسب تمرینات قدرتی پیشرفته و باشگاه‌های تخصصی",
   },
